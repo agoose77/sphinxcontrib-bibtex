d9dfdb78c0fb0268cb740af365ffbc631494db1e
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2022-04-06 14:19:49 +0100

    Regression test for numpydoc backrefs (see issue #277).

--- test/test_numpydoc.py
+++ test/test_numpydoc.py
@@ -1,5 +1,6 @@
 from test.common import (
-    html_citation_refs, html_citations, html_footnote_refs, html_footnotes
+    html_citation_refs_single, html_citations, html_footnote_refs,
+    html_footnotes
 )
 import pytest
 
@@ -20,14 +21,15 @@ def test_numpydoc(app, warning) -> None:
     titles = ['Een', 'Twee', 'Drie', 'Vier', 'Vijf', 'Zes', 'Zeven', 'Acht',
               'Negen', 'Tien', 'Elf']
     for label, title in zip(labels, titles):
-        assert len(html_citation_refs(label=label).findall(output)) == 1
+        assert len(html_citation_refs_single(label=label).findall(output)) == 1
         assert len(html_citations(label=label).findall(output)) == 1
-        match_ref = html_citation_refs(label=label).search(output)
+        match_ref = html_citation_refs_single(label=label).search(output)
         match = html_citations(label=label).search(output)
         assert match_ref
         assert match
         assert match_ref.group('refid') == match.group('id_')
         assert title in match.group('text')
+        assert match_ref.group('id_') == match.group('backref')
     output2 = (app.outdir / "doc_footcite.html").read_text()
     assert len(html_footnote_refs().findall(output2)) == 11
     for title in titles:
